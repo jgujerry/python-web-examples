88c0f31803dd22d89417d74b800fb61fbea89e1e
jgujerry <jianligu3@gmail.com>
2024-03-16 22:15:41 -0600

    Update pyramid

--- pyramid-html/pyramid_html/views/default.py
+++ pyramid-html/pyramid_html/views/default.py
@@ -1,6 +1,17 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='pyramid_html:templates/mytemplate.jinja2')
-def my_view(request):
+@view_config(
+    route_name='home',
+    renderer='pyramid_html:templates/home.jinja2'
+)
+def home(request):
     return {'project': 'pyramid-html'}
+
+
+@view_config(
+    route_name='about',
+    renderer='pyramid_html:templates/about.jinja2'
+)
+def about(request):
+    return {'message': 'About pyramid.'}
--- pyramid-html/tests/test_views.py
+++ pyramid-html/tests/test_views.py
@@ -1,13 +1,20 @@
-from pyramid_html.views.default import my_view
+from pyramid_html.views.default import home, about
 from pyramid_html.views.notfound import notfound_view
 
 
-def test_my_view(app_request):
-    info = my_view(app_request)
+def test_home(app_request):
+    info = home(app_request)
     assert app_request.response.status_int == 200
     assert info['project'] == 'pyramid-html'
+
 
 def test_notfound_view(app_request):
     info = notfound_view(app_request)
     assert app_request.response.status_int == 404
     assert info == {}
+
+
+def test_about(app_request):
+    info = about(app_request)
+    assert app_request.response.status_int == 200
+    assert info['message'] == 'About pyramid.'
