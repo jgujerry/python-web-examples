8379d62df30a20bd9972af25ee1b0256667e75d0
jgujerry <jianligu3@gmail.com>
2024-03-19 00:21:22 -0600

    Update streamlit openai app

--- streamlit-openai/src/streamlit_openai/main.py
+++ streamlit-openai/src/streamlit_openai/main.py
@@ -1,13 +1,11 @@
-import os
-
 import openai
 import streamlit as st
 
 
 st.title("Stream OpenAI Chat App")
-openai.api_key = os.getenv("OPAI_API_KEY")
+openai.api_key = st.secrets["openai_api_key"]
 
-MODEL_NAME = 'gpt-3.5-turbo'
+MODEL_NAME = "gpt-3.5-turbo"
 
 
 if "messages" not in st.session_state:
@@ -41,8 +39,9 @@ if prompt := st.chat_input():
             stream=True
         )
         for response in completions:
-            full_response += response.choices[0].delta.get("content", "")
-            message_placeholder.markdown(full_response + "|")
+            content = response.choices[0].delta.content or ""
+            full_response += content
+            message_placeholder.markdown(full_response)
         
         message_placeholder.markdown(full_response)
     
