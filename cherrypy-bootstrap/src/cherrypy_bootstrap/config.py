bf90c841190bd3c415c67d9a4245e721abd80b05
jgujerry <jianligu3@gmail.com>
2024-04-02 21:07:59 -0600

    Update config

--- cherrypy-bootstrap/src/cherrypy_bootstrap/config.py
+++ cherrypy-bootstrap/src/cherrypy_bootstrap/config.py
@@ -4,7 +4,20 @@ import cherrypy
 
 
 path = pathlib.Path(__file__).parent.absolute()
+
+
 app_config = {
+  "/": {
+        "tools.sessions.on": True,
+        "tools.staticdir.root": "/path/to/static/files",
+    },
+  "/static" : {
+      "tools.staticdir.on"  : True,
+      "tools.staticdir.dir" : path.joinpath("templates")
+  }
+}
+
+global_config = {
   "global" : {
     "server.socket_host" : "127.0.0.1",
     "server.socket_port" : 8080,
@@ -14,20 +27,17 @@ app_config = {
 
     "tools.trailing_slash.on" : False
   },
-  "/static" : {
-    "tools.staticdir.on"  : True,
-    "tools.staticdir.dir" : path.joinpath("templates")
-  }
+  
 }
 
 if cherrypy.config.get("environment") == "production":
-  app_config["global"]["engine.autoreload.on"] = False
-  app_config["global"]["server.thread_pool"] = 30
-  app_config["global"]["server.socket_host"] = "0.0.0.0"
-  app_config["global"]["server.socket_port"] = 80
-  app_config["global"]["environment"] = "production"
-  app_config["global"]["log.screen"] = False
+  global_config["global"]["engine.autoreload.on"] = False
+  global_config["global"]["server.thread_pool"] = 30
+  global_config["global"]["server.socket_host"] = "0.0.0.0"
+  global_config["global"]["server.socket_port"] = 80
+  global_config["global"]["environment"] = "production"
+  global_config["global"]["log.screen"] = False
 
 else:
-  app_config["global"]["environment"] = "development"
-  app_config['global']['log.screen'] = True
+  global_config["global"]["environment"] = "development"
+  global_config['global']['log.screen'] = True
